bc8ae45c9bd2ae1a2896975a617bb6da64717727
Dominic Tremblay <dominictremblay24@gmail.com>
2020-04-28 13:14:08 -0400

    added final version

--- sum.js
+++ sum.js
@@ -4,55 +4,75 @@
 // skip it. Do support negative numbers though. If any argument
 // is not a number, output an error message. We need at least 2 arguments.
 
-
 // take in an unlimited number of command line arguments
-const args = process.argv.slice(2);
 
-
-// console.log(args);
-// We need at least 2 arguments.
-if (args.length < 2) {
-  // print error message
-  console.log("Please provide at least 2 arguments");
-  // stop the excution of the js
-  // return;
-  process.exit(1);
-}
-
-
-// goe through each number 
-
-let total=0;
-
-for (let i=0; i < args.length; i ++) {
-  // Add each number to an accumulator
-  // console.log("index:", i, "value", args[i]);
-
-  // Convert the args to numbers
-  const nb = Number(args[i]);
-
-  console.log("NB:", nb);
-
-  // If any argument is not a number, output an error message.
-  if (isNaN(nb)) {
-    console.log('Please provide only numbers');
+const getArguments = function () {
+  const args = process.argv.slice(2);
+  // We need at least 2 arguments.
+  if (args.length < 2) {
+    // print error message
+    console.log('Please provide at least 2 arguments');
+    // stop the excution of the js
+    // return;
     process.exit(1);
-  } 
-
-
-  // If any argument is not a whole number, skip it
-  // if it is a whole nb, add it, otherwise do nothing
-  if (nb % 1 === 0) {
-    total += nb;
   }
-}
 
+  return args;
+};
 
+validateAllNums = function (list) {
+  for (let i = 0; i < list.length; i++) {
+    // If any argument is not a number, output an error message.
+    if (isNaN(list[i])) {
+      // console.log('Please provide only numbers');
+      console.log('Please provide only numbers');
+      process.exit(1);
+      // return ;
+    }
+  }
 
+  return list;
+};
 
+const convertToNums = function (list) {
+  const outputArr = [];
 
+  for (let i = 0; i < list.length; i++) {
+    // Convert the args to numbers
+    outputArr.push(Number(list[i]));
+  }
 
+  return outputArr;
+};
 
+const validateAllInt = function (numbersArr) {
+  const outputArr = [];
+
+  for (let i = 0; i < numbersArr.length; i++) {
+    // If any argument is not a whole number, skip it
+    if (numbersArr[i] % 1 === 0) {
+      outputArr.push(numbersArr[i]);
+      // console.log("whole nb", numbersArr[i])
+    }
+  }
+
+  return outputArr;
+};
+
+const sum = function (numbersArr) {
+  // goe through each number
+  console.log('numbersArr', numbersArr);
+  let total = 0;
+
+  for (let i = 0; i < numbersArr.length; i++) {
+    // Add each number to an accumulator
+    total += numbersArr[i];
+  }
+
+  return total;
+};
 
 // print out the sum of them.
-console.log(total);
+console.log(
+  sum(validateAllInt(validateAllNums(convertToNums(getArguments()))))
+);
