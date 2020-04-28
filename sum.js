b4262db90e285b505074042efda898a0e2738a0a
Dominic Tremblay <dominictremblay24@gmail.com>
2020-04-28 11:06:29 -0400

    retrieve args from cmd line, created the loop

--- sum.js
+++ sum.js
@@ -6,3 +6,22 @@
 
 
 // take in an unlimited number of command line arguments
+const args = process.argv.slice(2);
+
+// We need at least 2 arguments.
+
+// goe through each number 
+
+for (let i=0; i < args.length; i ++) {
+  // Add each number to an accumulator
+  console.log("index:", i, "value", args[i]);
+}
+
+
+// If any argument is not a whole number, skip it
+
+// If any argument is not a number, output an error message.
+
+
+
+// print out the sum of them.
