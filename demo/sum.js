92a3eeea939c1f19a139fa2844d730e9e140f19c
Dominic Tremblay <dominictremblay24@gmail.com>
2021-01-05 13:08:50 -0500

    demo final

--- demo/sum.js
+++ demo/sum.js
@@ -1,22 +1,29 @@
-// - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
+// Write a node program that takes in an unlimited number of command line arguments,
+// goes through each and prints out the sum of them. If any argument is not a whole number, skip it.
+// Do support negative numbers though.
+// If any argument is not a number, output an error message. We need at least 2 arguments.
 
-// takes in an unlimited number of command line arguments
-// for loop?
-// process.argv
+// Version control
+// Create a github repo...
+// create branch
+// Make regular commits
 
 const getArguments = function () {
+  // takes in an unlimited number of command line arguments
   const args = process.argv.slice(2);
+
   // We need at least 2 arguments.
+
   if (args.length < 2) {
     console.log('Please provide at least 2 arguments');
-    process.exit(1);
+    process.exit();
   }
+
   return args;
 };
 
 const convertToNums = function (numbers) {
   const output = [];
-
   for (let i = 0; i < numbers.length; i++) {
     output.push(Number(numbers[i]));
   }
@@ -25,57 +32,43 @@ const convertToNums = function (numbers) {
 
 const validateAllNums = function (numbers) {
   for (let i = 0; i < numbers.length; i++) {
-    // If any argument is not a number, output an error message.
+    // If any argument is not a number, output an error message
+    // check if the number is NaN
+
     if (isNaN(numbers[i])) {
       console.log('Please provide only numbers');
-      process.exit(1);
+      process.exit();
     }
   }
+
   return numbers;
 };
 
-const getAllInt = function (numbers) {
-  const output = [];
-
-  for (let i = 0; i < numbers.length; i++) {
-    // If any argument is not a whole number, skip it.
-
-    // add the currentNumber to total
-    // total <= total + currentNumber
-    if (Math.floor(numbers[i]) === numbers[i]) {
-      output.push(numbers[i]);
-    }
-  }
-
-  return output;
-};
-
 const sum = function (numbers) {
-  // goes through each number and add them up
+  // input => array of numbers
 
-  // declare an accumulator
-
+  // create an accumulator (total)
   let total = 0;
 
-  // c-style for loop
+  // goes through each
   for (let i = 0; i < numbers.length; i++) {
-    total += numbers[i];
+    // converting to number
+    // const number = Number(numbers[i]);
 
-    console.log(
-      'Index:',
-      i,
-      'Current Number',
-      numbers[i],
-      'type:',
-      typeof numbers[i]
-    );
+    const number = numbers[i];
+
+    // Add them up to the total
+    // If any argument is not a whole number, skip it.
+    if (number % 1 === 0) {
+      total += number;
+    }
+
+    console.log('index:', i, 'number:', numbers[i], 'total:', total);
   }
 
+  // return the total
   return total;
 };
 
-// prints out the sum of them.
-
-const result = sum(getAllInt(validateAllNums(convertToNums(getArguments()))));
-
+const result = sum(validateAllNums(convertToNums(getArguments())));
 console.log(result);
